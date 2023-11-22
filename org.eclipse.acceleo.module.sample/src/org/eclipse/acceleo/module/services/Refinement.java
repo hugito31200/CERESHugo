da4d6bb8ef9eb4be6977026c6f08a755ca510c80
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2023-11-22 10:47:09 +0100

    Added the version information in the MTT programs

--- org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/services/Refinement.java
+++ org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/services/Refinement.java
@@ -4,19 +4,25 @@ import org.eclipse.uml2.uml.Class;
 
 public class Refinement {
 	
-	private String[] cpeNames= {"ecostruxure_control_expert","ecostruxure_machine_expert","ecostruxure_opcua_server"};
+	private String[] cpeNames= {"ecostruxure_control_expert","ecostruxure_machine_expert","ecostruxure_opcua_server_expert"};
 	
 	
 	
-	public String refine(Class c) {
+	public String refine(Class c, String versionNumber) {
 		
 		String result="";
 		String formattedName = c.getName().replaceAll(" ", "_").toLowerCase();
+		versionNumber = versionNumber.replaceAll("\\.","_");
+		
 		for(int i=0;i<cpeNames.length;i++) {
 			String cpeName = cpeNames[i];
 			if(cpeName.contains(formattedName)) {
+				if(versionNumber.equals("0_0")) {
+					result+="isA("+formattedName+", "+cpeName+").\n";
+				}else {
+					result+="isA("+formattedName+", "+cpeName+"_"+versionNumber+").\n";
+				}
 				
-				result+="isA("+formattedName+", "+cpeName+").\n";
 			}
 		}
 		
