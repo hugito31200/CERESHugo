da65f1ac8c05b6f88350312c0c3ba4e91142a9cf
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2023-11-28 10:08:06 +0100

    Added back CVSS information which was lost after the merge

--- mulval/utils/runOperational.sh
+++ mulval/utils/runOperational.sh
@@ -2,6 +2,8 @@
 
 cat $1 /tool/mulval/lib/refinement.P > temp.P 
 
-graph_gen.sh temp.p -v -p -a /tool/mulval/SCADA/operationalRules.P
+graph_gen.sh temp.p -v -p --cvss -a /tool/mulval/SCADA/operationalRules.P > /dev/null
 
 rm temp.P
+
+probAssess.sh
