48598ee42b63a66adecef17b11253a6665380a31
hugito31200 <hugo.teixeiradecastro@hotmail.com>
2023-12-05 11:19:34 +0100

    Changes on vulnerability refinement
    -Added 2 vulnerabilities for MySQL server
    -Added a predicate "abstractVul" to differentiate between concrete vulnerabilities

--- mulval/utils/runOperational.sh
+++ mulval/utils/runOperational.sh
@@ -2,7 +2,7 @@
 
 cat $1 /tool/mulval/lib/refinement.P > temp.P 
 
-graph_gen.sh temp.p -v -p --cvss -a /tool/mulval/SCADA/operationalRules.P > /dev/null
+graph_gen.sh temp.p -v -p --cvss -a /tool/mulval/SCADA/operationalRules.P 
 
 rm temp.P
 
